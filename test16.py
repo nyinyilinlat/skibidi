e8ebcb8d7b773d2e75025804c7d5883f98744d91
nyinyilinlat <zwelinux.demo@gmail.com>
2024-04-30 12:01:56 +0700

    claer

---
+++ test16.py
@@ -0,0 +1,16 @@
+user_info = {
+    'User' : 'Nyi Nyi Lin Lat',
+    'Height' : "5 8",
+    'Fav_foods' : {
+         'food1' : 'Tha min'
+         'food2' : 'noodles slat'
+         'food3' : 'Sea food'
+    },
+    'fav_song' : {
+        'song1' : 'Smoke it off'
+        'song2' : 'Push Push Push'
+        'song3'  : '505'
+    }
+}
+
+print(user_info['fav_song']['song1'])

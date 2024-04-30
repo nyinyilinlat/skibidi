86aa0a503182a0907f60e67cee72ef829b4fcc9b
nyinyilinlat <zwelinux.demo@gmail.com>
2024-04-30 12:25:11 +0700

    skibidi

--- test16.py
+++ test16.py
@@ -2,13 +2,13 @@ user_info = {
     'User' : 'Nyi Nyi Lin Lat',
     'Height' : "5 8",
     'Fav_foods' : {
-         'food1' : 'Tha min'
-         'food2' : 'noodles slat'
-         'food3' : 'Sea food'
+         'food1' : 'Tha min',
+         'food2' : 'noodles slat',
+         'food3' : 'sea food'
     },
     'fav_song' : {
-        'song1' : 'Smoke it off'
-        'song2' : 'Push Push Push'
+        'song1' : 'smoke it off',
+        'song2' : 'push push push',
         'song3'  : '505'
     }
 }
---
+++ test20.py
@@ -0,0 +1,6 @@
+from test19 import user_info
+
+
+user_info("zwe", "zwelinux@gmail.com", "Chicago")
+
+user_info(addr="Yangon", name="Nyi",mail="nyi@hotmail.com")
